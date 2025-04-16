58df30560fd38f0e8fddf1d54b899293658d4801
Anurudha Sarkar <132932624+anurudha07@users.noreply.github.com>
2025-04-16 15:44:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,13 +6,11 @@ const weatherLocation = document.getElementById('weather-location');
 const weatherDescription = document.getElementById('weather-description');
 const weatherTemperature = document.getElementById('weather-temperature');
 const weatherTime = document.getElementById('weather-time');
-
 weatherBtn.addEventListener('click', () => {
   // Use the OpenWeatherMap API to fetch weather data
   const apiKey = 'f85e072ff99a8c16be6da49bded55bcf';
   const city = 'Kolkata';
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
   fetch(url)
     .then(response => {
       console.log('API Response:', response);
@@ -26,9 +24,10 @@ weatherBtn.addEventListener('click', () => {
       if (!data || !data.name || !data.weather || !data.main) {
         throw new Error('Invalid API response');
       }
+      const temperatureInCelsius = data.main.temp - 273.15;
       weatherLocation.textContent = data.name;
       weatherDescription.textContent = data.weather[0].description;
-      weatherTemperature.textContent = `Temperature: ${data.main.temp}°C`;
+      weatherTemperature.textContent = `Temperature: ${temperatureInCelsius.toFixed(2)}°C`;
       weatherTime.textContent = `Time: ${new Date().toLocaleTimeString()}`;
       weatherModal.style.display = 'block';
     })
@@ -41,6 +40,7 @@ weatherBtn.addEventListener('click', () => {
       weatherModal.style.display = 'block';
     });
 });
+
 
 closeWeatherModal.addEventListener('click', () => {
   weatherModal.style.display = 'none';
